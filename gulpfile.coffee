1e83797f8d1ac5cb38733f42992e3380f8863809
Jason Dixon <jason.dixon.email@gmail.com>
2017-01-24 00:49:17 +1300

    adding some build tools

--- gulpfile.coffee
+++ gulpfile.coffee
@@ -1,1 +1,11 @@
 # Lets make a build process!
+
+gulp = require 'gulp'
+coffee = require "gulp-coffee"
+jsmin = require "gulp-jsmin"
+
+gulp.task "coffee", ()->
+  gulp.src "./js/*.coffee"
+  .pipe coffee()
+  .pipe jsmin()
+  .pipe gulp.dest "./build/js"
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,14 @@
 (function() {
+  var coffee, gulp, jsmin;
 
+  gulp = require('gulp');
+
+  coffee = require("gulp-coffee");
+
+  jsmin = require("gulp-jsmin");
+
+  gulp.task("coffee", function() {
+    return gulp.src("./js/*.coffee").pipe(coffee()).pipe(jsmin()).pipe(gulp.dest("./build/js"));
+  });
 
 }).call(this);
